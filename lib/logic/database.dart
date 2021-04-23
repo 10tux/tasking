ead6cce62dc6e0367d99058f6b83baa442ad92f8
sandeep v <setsuna.cyan@outlook.com>
2021-04-23 22:34:45 +0530

    sqlite dll file loading and database checking.

--- lib/logic/database.dart
+++ lib/logic/database.dart
@@ -6,28 +6,58 @@ import 'dart:ffi';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:sqlite3/sqlite3.dart';
 import 'package:sqlite3/open.dart';
 
-/// Get sqlite3 dynamic library
-DynamicLibrary _openOnWindows() {
-  File dllFile;
+String getFilesDirectory() {
   if (kDebugMode) {
-    dllFile = File(
-        '${Directory.current.path}\\build\\windows\\runner\\Debug\\sqlite3.dll');
+    return '${Directory.current.path}\\build\\windows\\runner\\Debug';
   } else {
-    dllFile = File('${Directory.current.path}\\sqlite3.dll');
+    return '${Directory.current.path}';
   }
-  return DynamicLibrary.open(dllFile.path);
 }
 
-void openAndCloseDb() {
-  open.overrideFor(OperatingSystem.windows, _openOnWindows);
-  final db = sqlite3.openInMemory();
-  db.dispose();
+class DatabaseProvider extends ChangeNotifier {
+  DatabaseStatus _status = DatabaseStatus.NotLoaded;
+
+  DatabaseProvider() {
+    _overrideDefaultLoadingBehaviour();
+  }
+
+  get dbStatus => _status;
+  set dbStatus(DatabaseStatus s) {
+    _status = s;
+    notifyListeners();
+  }
+
+  /// Get dynamic library for sqlite3
+  DynamicLibrary _openOnWindows() {
+    File dllFile;
+    final filesDir = getFilesDirectory();
+    dllFile = File('$filesDir\\sqlite3.dll');
+    return DynamicLibrary.open(dllFile.path);
+  }
+
+  /// override default loading behaviour
+  void _overrideDefaultLoadingBehaviour() {
+    open.overrideFor(OperatingSystem.windows, _openOnWindows);
+  }
+
+  /// check if database file exists
+  static Future<bool> checkIfDatabaseExists() async {
+    File dbFile = File('${getFilesDirectory()}\\tasking.db');
+    if (await dbFile.exists()) {
+      return true;
+    }
+    return false;
+  }
+
+  static createDatabase() {
+    
+  }
 }
 
-Database getSQLiteDatabase(String filePath) {
-  open.overrideFor(OperatingSystem.windows, _openOnWindows);
-  return sqlite3.open(filePath);
-}
+enum DatabaseStatus { Loaded, NotLoaded, FileMissing }
+
+/// Get sqlite3 dynamic library
+
+void openAndCloseDb() {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,31 @@
 import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'screens/home_screen.dart';
+import 'logic/database.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   DesktopWindow.setMinWindowSize(Size(1000, 1000));
-  runApp(MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => DatabaseProvider()),
+    ],
+    child: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+/// Root widget for application
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+/// State for `MyApp`
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -18,7 +35,55 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'Lexend',
       ),
-      home: MyHomePage(title: 'Tasking'),
+      home: Consumer<DatabaseProvider>(
+        builder: (context, dbProvider, child) {
+          if (dbProvider.dbStatus == DatabaseStatus.Loaded) {
+            return MyHomePage(title: 'Tasking');
+          } else {
+            return LoadingPage();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class LoadingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  void initState() {
+    super.initState();
+    _loadDatabase();
+  }
+
+  void _loadDatabase() async {
+    // check if database file exists
+    if (await DatabaseProvider.checkIfDatabaseExists()) {
+      //pass
+    } else {
+      Provider.of<DatabaseProvider>(context, listen: false).dbStatus =
+          DatabaseStatus.FileMissing;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Consumer<DatabaseProvider>(
+          builder: (context, dbProvider, child) {
+            if (dbProvider.dbStatus == DatabaseStatus.FileMissing) {
+              return Text('Database File Missing');
+            } else {
+              return Text('Database Loading');
+            }
+          },
+        ),
+      ),
     );
   }
 }
